f7a872b63f49a17f90ada843d638d4a69b9ab9dc
CipherKode <kodecipher@gmail.com>
2026-01-24 16:58:54 +0530

    snehal : removed the usage of context

--- components/ui/shell.tsx
+++ components/ui/shell.tsx
@@ -8,17 +8,13 @@ import { BlogSheet } from "./blog-sheet"
 import { useMenu } from "@/contexts/MenuContext"
 
 export default function Shell({ children }: { children: React.ReactNode }) {
-  const { blogOpen, setBlogOpen } = useMenu()
 
   return (
     <div className="min-h-screen bg-background font-sans text-foreground dark:bg-black">
       <TopNav showSecondRow={true} />
       <Sidebar />
 
-      <BlogSheet open={blogOpen} onOpenChange={setBlogOpen} />
-
       <div className="mx-auto max-w-3xl px-4 pt-24 pb-28">{children}</div>
-
 
       <Footer />
 
